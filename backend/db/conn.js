3fe06783697e75e196ff4f7fc3ec5d51e264a5a4
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-10-23 20:15:21 -0300

    Last Commit
    Last Commit

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/geolocalizacao");
-  console.log("Conectamos ao Mongoose!");
+  await mongoose.connect(
+    `mongodb+srv://robertoo7:IKtlq7AQPr2nnflP@cluster0.tcnpo.mongodb.net/Geo?retryWrites=true&w=majority&appName=Cluster0`
+  );
+  console.log("Conectamos ao MongoDB Atlas!");
 }
 
 main().catch((err) => console.log(err));
