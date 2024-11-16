f3d1c1dc36e79d4683134d1c022979359f1276a8
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2024-11-16 17:19:22 -0300

    Atualizando a API

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -27,8 +27,8 @@ const CheckOutPage = () => {
   });
   const [events, setEvents] = useState([]);
   const [selectedEventId, setSelectedEventId] = useState("");
-  const api = "http://localhost:3333";
-  /* https://geo-backend-aspq.onrender.com*/
+  const api = "https://geo-backend-aspq.onrender.com";
+  /* http://localhost:3333 */
 
   useEffect(() => {
     const fetchEvents = async () => {
